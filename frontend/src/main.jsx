c14d611cbd596b0c8d4a2e99d8ab93a405e000bc
HANU KOSHTI <95334298+HANU13@users.noreply.github.com>
2025-08-25 01:57:01 +0530

    Update main.jsx
    commenting import css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App"; 
-import "./index.css"; // optional for Tailwind or custom styles
+// import "./index.css"; // optional for Tailwind or custom styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
